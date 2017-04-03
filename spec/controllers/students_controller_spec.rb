b557c2b71431379f70e9c2f751c2b6cc749105d2
Junior <nog.junior84@gmail.com>
2017-04-03 00:22:27 -0300

    Routes, Specs

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe StudentsController, type: :controller do
+  describe "GET #show" do
+    it "assings the requested student to @student" do
+      student = create(:student)
+      get :show, params: { id: student, academy_id: student.academy_id }
 
+      expect(assigns(:student)).to eq student
+    end
+
+    it "renders the :show template" do
+      student = create(:student)
+      get :show, params: { id: student, academy_id: student.academy_id }
+
+      expect(response).to render_template :show
+    end
+  end
+
+  describe "GET #index" do
+    context "with params[:letter]" do
+      it "renders the :index template" do
+        academy = create(:academy)
+        get :index, params: { academy_id: academy }
+
+        expect(response).to render_template :index
+      end
+    end
+  end
 end
--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     document { Faker::Number.number(11)}
     email { Faker::Internet.safe_email }
 
-    factory :invalid_studen do
+    factory :invalid_student do
       name nil
     end
   end
