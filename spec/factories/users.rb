2c8df47866982a098a3a9385b1a5d3b42d063f73
Junior <nog.junior84@gmail.com>
2017-04-06 19:53:03 -0300

    Factory User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,41 @@ FactoryGirl.define do
     confirmed_at Date.today
 
     after(:create) { |user| user.add_role :admin }
+
+    factory :user_editor do
+      email { Faker::Internet.email }
+      password "password"
+      password_confirmation "password"
+      confirmed_at Date.today
+
+      after(:create) { |user| user.add_role :editor }
+    end
+
+    factory :user_author do
+      email { Faker::Internet.email }
+      password "password"
+      password_confirmation "password"
+      confirmed_at Date.today
+
+      after(:create) { |user| user.add_role :author }
+    end
+
+    factory :user_member do
+      email { Faker::Internet.email }
+      password "password"
+      password_confirmation "password"
+      confirmed_at Date.today
+
+      after(:create) { |user| user.add_role :member }
+    end
+
+    factory :user_guest do
+      email { Faker::Internet.email }
+      password "password"
+      password_confirmation "password"
+      confirmed_at Date.today
+
+      after(:create) { |user| user.add_role :guest }
+    end
   end
 end
